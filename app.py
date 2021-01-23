12c7c1d2ecdb725ecf32872c2b6c05bf59ceb3b1
ZachKopec <zachkopec@optonline.net>
2021-01-23 13:38:00 -0500

    Rename file for merge

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import json
+from flask_pymongo import PyMongo
 from flask import Flask, jsonify, render_template, redirect
+from bson.json_util import dumps
+from flask_pymongo import MonogoClient
+#from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
@@ -12,4 +17,14 @@ def html_templating():
     return render_template("html-templating.html")
 
 if __name__ == "__main__":
+    client = MongoClient()
+    db = client['Test']
+    collection = db['Squirrel']
+    cursor = collection.fin({})
+    with open('collection.json', 'w') as file:
+        file.write('[')
+        for document in cursor:
+            file.write(dumps(document))
+            file.write(',')
+        file.write(']')
     app.run(debug=True)
