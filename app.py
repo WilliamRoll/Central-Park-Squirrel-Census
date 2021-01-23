e7cceedef388f59cf5e53772bf0dd7a52965e449
ZachKopec <zachkopec@optonline.net>
2021-01-23 14:27:59 -0500

    Repurposed flask for project functionality

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import json
 from flask_pymongo import PyMongo
 from flask import Flask, jsonify, render_template, redirect
 from bson.json_util import dumps
-from flask_pymongo import MonogoClient
+from pymongo import MongoClient
 #from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
@@ -16,15 +15,33 @@ def home():
 def html_templating():
     return render_template("html-templating.html")
 
+@app.route("/heatmap.html")
+def heat_mapping():
+    return render_template("heatmap.html")
+
+@app.route("/scatter.html")
+def scatter_plot():
+    return render_template("scatter.html")
+
 if __name__ == "__main__":
-    client = MongoClient()
-    db = client['Test']
-    collection = db['Squirrel']
-    cursor = collection.fin({})
-    with open('collection.json', 'w') as file:
-        file.write('[')
-        for document in cursor:
-            file.write(dumps(document))
-            file.write(',')
-        file.write(']')
+    # client = MongoClient()
+    # db = client['Test']
+    # collection = db['Squirrel']
+    # cursor = collection.find({})
+    # with open('collection.json', 'w') as file:
+    #     file.write('[')
+    #     for document in cursor:
+    #         file.write(dumps(document))
+    #         file.write(',')
+    #     file.write(']')
     app.run(debug=True)
+
+# conn = 'mongodb://localhost:27017'
+# client = MongoClient(conn)
+# db = client['Test']
+# sqs = db['Squirrel']
+
+# mydoc = db.find(sqs)
+
+# for x in mydoc:
+#     print(x)
