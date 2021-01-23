257d0a75cf8200f4fa0e16d5113ca8682af25b0c
WilliamRoll <broll6595@gmail.com>
2021-01-23 13:54:42 -0500

    added basic heatmap code using web API

---
+++ heatmap/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiYnJvbGw2NTk1IiwiYSI6ImNranVseHFsMTAzcWYyc3F0YWhqN3MxZW0ifQ.xsqUWiTO9uehAEmgDM9icA";
--- mongo_connection.js
+++ mongo_connection.js
@@ -1,8 +1,3 @@
-var MongoClient = require('mongodb').MongoCLient;
-
 var url = 'mongodb://localhost/Squirrel_db'
 
-MongoClient.connect(url, function(err, db){
-
-    console.log()
-})
+console.log(url)
