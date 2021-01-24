e39c5cd7179b0e2712ab73cf9c8e351aa57aab82
Sherin M <sherinm@Sherins-MacBook-Pro.local>
2021-01-24 03:31:25 -0500

    updates

--- scatterplot/app.js
+++ scatterplot/app.js
@@ -21,49 +21,65 @@ var height = svgHeight - margin.top - margin.bottom;
 
 // svg wrapper
 var svg = d3
-    .select(".chart")
+    .select(".#scatter")
     .append("svg")
     .attr("width", "svgWidth")
     .attr("height", "svgHeight")
-
 
 //append
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-
-
 // retrieving test.json data
-d3.json("test.json").then(function(testData)
-
-    {
-    
-        testData.forEach(function(data){
-            data.primary_fur_color = +data.primary_fur_color;
-            data.chasing = +data.chasing;
+d3.json("test.json").then(function(testData) {
+    testData.forEach(function(data){
+        data.primary_fur_color = +data.primary_fur_color;
+        data.chasing = +data.chasing;
 
 // scalar function
-        var xLinearScale = d3.scaleLinear()
-     	    .domain([20, d3.max(testData, d => d.primary_fur_color)])
-      	    .range([0, width]);
+    var xLinearScale = d3.scaleLinear()
+     	.domain([20, d3.max(testData, d => d.primary_fur_color)])
+      	.range([0, width]);
 
-       var yLinearScale = d3.scaleLinear()
-     	    .domain([0, d3.max(testData, d => d.chasing)])
-            .range([height, 0]);
+    var yLinearScale = d3.scaleLinear()
+     	.domain([0, d3.max(testData, d => d.chasing)])
+        .range([height, 0]);
 
 // axis functions
-        var bAxis = d3.axisBottom(xLinearScale);
-        var lAxis = d3.axisLeft(yLinearScale);
+    var bAxis = d3.axisBottom(xLinearScale);
+    var lAxis = d3.axisLeft(yLinearScale);
 
 // Appending axes 
 
-        chartGroup.append("g")
-            .attr("transform", `translate(0, ${height})`)
-            .call(bAxis);
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bAxis);
 
-        chartGroup.append("g")
-            .call(lAxis);
-             
+    chartGroup.append("g")
+        .call(lAxis);
+// circles
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(testData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.primary_fur_color))
+        .attr("cy", d => yLinearScale(d.chasing))
+        .attr("r", "15")
+        .attr("fill", "green")
+        .attr("opacity", ".3");
+// tooltip
+    var tTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`${d.unique_squirrel_id}<br> Primary Fur Color : ${d.primary_fur_color}<br>Chasing: ${d.chasing}`);
+        });
+        
+    chartGroup.call(tTip);
+
+
+        
+});
     });
 
 
