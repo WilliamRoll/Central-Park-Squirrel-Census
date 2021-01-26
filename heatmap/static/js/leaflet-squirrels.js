ce06ca192341b9b4ba6777d8ef6b1f5eb0359256
Kinnari Patel <you@example.com>
2021-01-26 14:38:17 -0500

    updating

---
+++ heatmap/static/js/leaflet-squirrels.js
@@ -0,0 +1,63 @@
+function createMap(squirrels) {
+
+    // Create the tile layer that will be the background of our map
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "light-v10",
+      accessToken: API_KEY
+    });
+  
+    // Create a baseMaps object to hold the lightmap layer
+    var baseMaps = {
+      "Light Map": lightmap
+    };
+  
+    // Create an overlayMaps object to hold the bikeStations layer
+    var overlayMaps = {
+      "Squirrels Location": squirrels
+    };
+  
+    // Create the map object with options
+    var map = L.map("map-id", {
+      center: [40.73, -74.0059],
+      zoom: 12,
+      layers: [lightmap, squirrels]
+    });
+  
+    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(map);
+  }
+ 
+d3.json("/raw-web-api", function (mydata) { 
+    response = mydata
+    console.log(response); 
+    function createMarkers(response) {
+
+        // Pull the "stations" property off of response.data
+        var location = response.geocoded_column;
+    
+        // Initialize an array to hold bike markers
+        var locationArray = [];
+        var colorArray = [];
+        var ChasingArray = [];
+        // Loop through the stations array
+        for (var index = 0; index < response.length; index++) {
+        var location = response[index].geocoded_column;
+        var color = response[index].primary_fur_color;
+        var chasing = response[index].chasing;
+        // For each station, create a marker and bind a popup with the station's name
+        var squirrelMarker = L.marker([location.coordinates[1], location.coordinates[0]])
+            .bindPopup("<h3>" + color + "<h3><h3>Approaching " + chasing + "</h3>");
+    
+        // Add the marker to the bikeMarkers array
+        locationArray.push(squirrelMarker);
+        }
+    
+        // Create a layer group made from the bike markers array, pass it into the createMap function
+        createMap(L.layerGroup(locationArray));
+    }
+    createMarkers(response);
+});
