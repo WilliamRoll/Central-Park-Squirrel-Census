22375795dcef44d1fed9d8359194c9a7da283420
Rachel Chan <70292238+chanrce@users.noreply.github.com>
2021-01-27 19:54:24 -0500

    Converted to array

--- static/js/scatter_dictionaries.js
+++ static/js/scatter_dictionaries.js
@@ -1,42 +1,43 @@
-// // area dimensions
-// var svgWidth = 900;
-// var svgHeight = 660;
+// barchart??
 
-// //margin for charts are here 
-// var margin = {
-//     top: 30,
-//     right: 30,
-//     bottom: 30,
-//     left: 30
-// };
+var svgWidth = 900;
+var svgHeight = 460;
 
-// var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-// var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+//margin for charts are here 
+var margin = {
+    top: 40,
+    right: 40,
+    bottom: 40,
+    left: 40
+};
 
-// // svg wrapper
-// var svg = d3
-//     .select("#scatter")
-//     .append("svg")
-//     .attr("width", svgWidth)
-//     .attr("height", svgHeight)
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
 
-// //append
-// var chartGroup = svg.append("g")
-//   .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+// svg wrapper
+var svg = d3
+    .select("#color_barchart")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
 
-// retrieving test.json data
+//append
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+
+
 d3.json("/raw-web-api", function (myData) { 
     data = myData
     console.log(data); 
 
-    // color_arr = []
-    // chase_arr = []
-    // var gray_true_dict=[];
+
 
     var gray_approach_count=0
     var cinnamon_approach_count=0
     var black_approach_count=0
-    // var gray_true_dict =[]
+
 
     // loop
     for (var index = 0; index < data.length; index++){
@@ -49,79 +50,29 @@ d3.json("/raw-web-api", function (myData) {
         }
         else if (approach=="True" && primary_color=="Cinnamon") {
             cinnamon_approach_count+=1
-          }
+            }
         else if (approach=="True" && primary_color=="Black") {
             black_approach_count+=1
         }
 
-        // if (approach="True"){
-        //     colors_true_dict.push({
-        //         key: primary_color,
-        //         value: approach
-        //     })
-        // }
-
-        // if (approach=="True" && primary_color=="Gray"){
-        //     gray_true_dict.push({
-        //         key: "Gray",
-        //         value: gray_approach_count
-
-        //     })
-        // }
+        
     }
-    // console.log(gray_true_dict)
-    console.log(gray_approach_count)
+
 
     var true_dict = {
         "Gray": gray_approach_count,
         "Cinnamon": cinnamon_approach_count,
         "Black": black_approach_count
     };
-
     console.log(true_dict)
+    
+    // converting data into array
+    const true_array = Object.entries(true_dict).map(([key, value]) => ({
+    key: key,
+    value: value
+    }));
+    
+    console.log(true_array)
 
-    // console.log(colors_true_dict)
 
-    // // loop
-    // data.forEach(function(dataSet){
-    //     approach=dataSet.approaches
-    //     primary_color = dataSet.primary_fur_color; 
-        // console.log(primary_color)
-        
-        // //Counters for approach and color
-        // if(approach=="True" && primary_color=="Gray"){
-        //     gray_approach_count+=1
-        // }
-        // else if (approach=="True" && primary_color=="Cinnamon") {
-        //     cinnamon_approach_count+=1
-        //   }
-        // else if (approach=="True" && primary_color=="Black") {
-        //     black_approach_count+=1
-        // }
-        
-
-    // primary_color = dataSet.primary_fur_color;
-    // chase = dataSet.chasing;
-    // color_arr.push(primary_color)
-    // chase_arr.push(chase)
-    // });
-    // console.log(color_arr);
-    // console.log(chase_arr);
     });
-    // console.log(gray_approach_count)
-    // console.log(cinnamon_approach_count)
-    // console.log(black_approach_count)
-// });
-
-//Counter only works inside the loop...not sure how this will work with visualizations
-
-// console.log(cinnamon_approach_count)
-// console.log(black_approach_count)
-
-
-
-
-
-
-
-
