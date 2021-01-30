b2c300b268d82a3214ea24d73a79a078cf2f45a7
Kinnari Patel <you@example.com>
2021-01-30 13:05:01 -0500

    updating

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -3,7 +3,7 @@ var myMap = L.map("map", {
   center: [40.79, -73.96],
   zoom: 14.44
 });
-
+// Adding tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -13,14 +13,16 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-
+// Use this link to get the mongo data from the API created.
+// Grabbing our data into the function..
 d3.json("/raw-web-api", function (mydata) { 
   data = mydata
   console.log(data); 
 
+  // creating an empty array to grab all the coordinates of the squirrels
   var locationArray = [];
-  var colorArray = [];
-  var ChasingArray = [];
+
+  // using a loop and running through the length of the database to grab everything
   for (var i = 0; i < data.length; i++) {
     var location = data[i].geocoded_column;
     var color = data[i].primary_fur_color;
@@ -28,16 +30,19 @@ d3.json("/raw-web-api", function (mydata) {
 
     if (location) {
       locationArray.push([location.coordinates[1], location.coordinates[0]]);
-      // L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
+    
     }
 
   }
+  // ploting the heat map
   var heat = L.heatLayer(locationArray, {
     radius: 20,
     blur: 35
   });
 
   myMap.addLayer(heat);
+
+  //viewing the data in console for futher use
 
   console.log(locationArray);
  
